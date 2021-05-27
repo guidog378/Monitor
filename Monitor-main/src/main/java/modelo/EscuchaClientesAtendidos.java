a518c18251f6d04c90ad567a29414fa2ce770309
guidog378 <guidogar123marg@gmail.com>
2021-05-27 19:45:11 -0300

    v13

--- Monitor-main/src/main/java/modelo/EscuchaClientesAtendidos.java
+++ Monitor-main/src/main/java/modelo/EscuchaClientesAtendidos.java
@@ -25,17 +25,19 @@ public class EscuchaClientesAtendidos implements Runnable {
 				this.conexion.enviarPaquete(peticion);
 				InfoClientesAtendidos clientesAtendidos =(InfoClientesAtendidos)this.conexion.recibirPaquete();
 				Stack<InfoClienteAtendido> clientes = clientesAtendidos.getPilaClientesAtendidos();
-				LinkedList<Integer> dnis = new LinkedList<Integer>();
-				LinkedList<Integer> box = new LinkedList<Integer>();
-				InfoClienteAtendido actual;
-				while(!clientes.isEmpty()) {
-					actual = clientes.pop();
-					dnis.add(actual.getDni());
-					box.add(actual.getBox());
+				if(clientes != null) {
+				     LinkedList<Integer> dnis = new LinkedList<Integer>();
+				     LinkedList<Integer> box = new LinkedList<Integer>();
+				     InfoClienteAtendido actual;
+				     while(!clientes.isEmpty()) {
+					     actual = clientes.pop();
+					     dnis.add(actual.getDni());
+					     box.add(actual.getBox());
+				     }
+				     this.window.getModeloTabla().setClientes(dnis);
+				     this.window.getModeloTabla().setBox(box);
+				     this.window.getTable().repaint();
 				}
-				this.window.getModeloTabla().setClientes(dnis);
-				this.window.getModeloTabla().setBox(box);
-				this.window.getTable().repaint();
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
