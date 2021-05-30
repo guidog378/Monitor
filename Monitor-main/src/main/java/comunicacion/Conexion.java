8885900158ff33e1aa373f25cdf29b11ccb2d51f
guidog378 <guidogar123marg@gmail.com>
2021-05-30 15:07:52 -0300

    v13

--- Monitor-main/src/main/java/comunicacion/Conexion.java
+++ Monitor-main/src/main/java/comunicacion/Conexion.java
@@ -8,24 +8,33 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import modeloInfo.InfoServerMonitor;
+import modeloInfo.InfoServerFuncional;
 import modeloInfo.Informable;
 
 public class Conexion {
 	private Socket socket;
+	private String ipServer,ipMonitor;
+	private int puertoServer,puertoMonitor;
 	
 	public Conexion() {
+		this.ipMonitor = "localhost";
+		this.puertoMonitor = 9090;
+		this.obtenerServidor();
 	}
 
 	public void enviarPaquete(Informable paqueteEnviar) {
 		try {
-			this.socket = new Socket("localhost",9797);
+			this.socket = new Socket(this.ipServer,this.puertoServer);
 			OutputStream fr = this.socket.getOutputStream();
 		    ObjectOutputStream ofr = new ObjectOutputStream(fr);
 		    ofr.writeObject(paqueteEnviar);
 		} catch (UnknownHostException e) {
-				e.printStackTrace();
+		    this.obtenerServidor();
+			this.enviarPaquete(paqueteEnviar);
 		} catch (IOException e) {
-				e.printStackTrace();
+			this.obtenerServidor();
+			this.enviarPaquete(paqueteEnviar);
 		}
 	}
 	
@@ -33,13 +42,31 @@ public class Conexion {
 		Informable paquete = null;
 		ObjectInputStream fe;
 		try {
-			fe = new ObjectInputStream(this.socket.getInputStream());
-			paquete = (Informable)fe.readObject();
+			InputStream is = this.socket.getInputStream();
+			fe = new ObjectInputStream(is);
+			paquete = (Informable)fe.readObject();	
 		} catch (IOException e) {
 			System.out.println("IOException");
 		} catch (ClassNotFoundException e) {
 			System.out.println("ClassNotFoundException");
 		} 
 		return paquete;
+	}
+	
+	private void obtenerServidor() {
+        try {
+        	InfoServerMonitor info = new InfoServerMonitor();
+			this.socket = new Socket(this.ipMonitor,this.puertoMonitor);
+			ObjectOutputStream oos = new ObjectOutputStream(this.socket.getOutputStream());
+			oos.writeObject(info);
+			ObjectInputStream ois = new ObjectInputStream(this.socket.getInputStream());
+			InfoServerFuncional infoServer = (InfoServerFuncional) ois.readObject();
+			this.ipServer = infoServer.getIpServer();
+			this.puertoServer = infoServer.getPuertoServer();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
---
+++ Monitor-main/src/main/java/modeloInfo/InfoServerFuncional.java
@@ -0,0 +1,27 @@
+package modeloInfo;
+
+public class InfoServerFuncional implements Informable {
+	private String IpServer;
+	private int puertoServer;
+
+	public int getIdOperacion() {
+		return 1;
+	}
+
+	public String getIpServer() {
+		return IpServer;
+	}
+
+	public void setIpServer(String ipServer) {
+		IpServer = ipServer;
+	}
+
+	public int getPuertoServer() {
+		return puertoServer;
+	}
+
+	public void setPuertoServer(int puertoServer) {
+		this.puertoServer = puertoServer;
+	}
+
+}
